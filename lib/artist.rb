3efbee8bef52057bb8f0760d7c35058ad87cd0ad
rtyenriques <tooshopboys@gmail.com>
2021-02-10 23:08:46 -0800

    commit tree 883602463c301c091d0222d3fbd7d7977c374fde

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,31 @@
+class Artist
+  attr_accessor :name, :songs, :genre
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def new_song(song, genre)
+    Song.new(song, self, genre)
+
+  end
+
+  def genres
+    songs.collect {|song| song.genre}
+  end
+
+
+
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
+  attr_accessor :name, :appointment, :doctors
+
+  @@all = []
+
+  def initialize(name)
+   @name = name
+   @@all << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+
+
+end
 
